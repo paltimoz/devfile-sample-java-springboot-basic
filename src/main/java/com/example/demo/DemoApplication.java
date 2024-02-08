788266b5f773dfc66c148730d0307e1758b200de
İzzet ÖzTürk <tr.ozturk.izzet@gmail.com>
2024-02-08 23:41:02 +0300

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,22 +1,48 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 
 @RestController
 @SpringBootApplication
 public class DemoApplication {
 
-    @RequestMapping("/")
+    @Autowired
+    RestTemplate restTemplate;
+
+    @RequestMapping("/hello")
     String home() {
         return "Hello World!";
+    }
+
+    @GetMapping("/test")
+    ResponseEntity<String> test(@RequestBody TestRequest request){
+        RestTemplate restTemplate = new RestTemplate();
+        return restTemplate.getForEntity(request.getUrl(), String.class);
     }
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
 
+}
+
+class TestRequest{
+    private String url;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
 }
